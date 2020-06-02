b4ab0c958b51259d1b7714c16fa3b975f2703ccd
Robert Ellis <rob@pigatron-industries.co.uk>
2020-06-02 12:29:12 +0100

    Added grid numbers where status bar was

--- sequence/src/interface/views/SequenceView.cpp
+++ sequence/src/interface/views/SequenceView.cpp
@@ -5,13 +5,16 @@
 #define DISPLAY_WIDTH 128
 #define DISPLAY_HEIGHT 128
 
+#define STATUS_HEIGHT 15
 #define CHANNEL_HEIGHT 14
 #define BAR_WIDTH 19
 
 #define VISIBLE_BARS DISPLAY_WIDTH / BAR_WIDTH + 1
 
 #define GRID_COLOUR Colour(20, 20, 20)
+#define GRID_TEXT_COLOUR Colour(127, 127, 127)
 #define DATA_COLOUR Colour(0, 40, 80)
+#define DATA_TEXT_COLOUR Colour(255, 255, 255)
 #define CURSOR_COLOUR Colour(255, 0, 0)
 
 SequenceView::SequenceView(AppData& _appData, Display& _display) :
@@ -24,6 +27,8 @@ SequenceView::SequenceView(AppData& _appData, Display& _display) :
 
 void SequenceView::render() {
     display.fillScreen(Colour(0, 0, 0));
+    display.setFont(Org_01);
+    display.setTextSize(1);
     renderStatusBar();
     renderGrid();
     renderSequence();
@@ -84,28 +89,30 @@ void SequenceView::cursorRight() {
 }
 
 void SequenceView::renderStatusBar() {
-    display.setTextSize(1);
     display.setTextColour(Colour(255, 255, 255));
-    display.setFont(Org_01);
-    display.setCursor(0, 12);
-    display.print("SELECT");
+    //display.setCursor(0, 12);
+    //display.print("SELECT");
 }
 
 void SequenceView::renderGrid() {
-    int16_t top = 15;
+    display.setTextColour(GRID_TEXT_COLOUR);
+    int16_t top = STATUS_HEIGHT;
     for(int16_t channel = 0; channel < SEQUENCE_CHANNELS+1; channel++) {
-        display.drawLine(0, top, 128, top, GRID_COLOUR);
+        display.drawLine(0, top, DISPLAY_WIDTH, top, GRID_COLOUR);
         top += CHANNEL_HEIGHT;
     }
     int16_t left = 0;
-    for(int16_t channel = 0; channel < SEQUENCE_CHANNELS+1; channel++) {
-        display.drawLine(left, 15, left, 128, GRID_COLOUR);
+    for(int16_t bar = scrollBar; bar < scrollBar+VISIBLE_BARS; bar++) {
+        display.drawLine(left, STATUS_HEIGHT, left, DISPLAY_HEIGHT, GRID_COLOUR);
+        display.setCursor(left, 12);
+        display.print(bar, HEX);
         left += BAR_WIDTH;
     }
 }
 
 void SequenceView::renderSequence() {
-    short top = 15;
+    display.setTextColour(DATA_TEXT_COLOUR);
+    short top = STATUS_HEIGHT;
     for(short channel = 0; channel < SEQUENCE_CHANNELS; channel++) {
         short left = 0;
         for(short bar = scrollBar; bar < scrollBar+VISIBLE_BARS; bar++) {
@@ -122,9 +129,9 @@ void SequenceView::renderSequence() {
 }
 
 void SequenceView::renderCursor() {
-    short top = 15+(cursorChannel*CHANNEL_HEIGHT);
+    short top = STATUS_HEIGHT+(cursorChannel*CHANNEL_HEIGHT);
     short left = (cursorBar-scrollBar)*BAR_WIDTH;
     display.drawRect(left, top, BAR_WIDTH+1, CHANNEL_HEIGHT+1, CURSOR_COLOUR);
     display.drawRect(left+1, top+1, BAR_WIDTH-1, CHANNEL_HEIGHT-1, CURSOR_COLOUR);
-    //display.drawLine(left, 15, left, 128, CURSOR_COLOUR);
+    //display.drawLine(left, STATUS_HEIGHT, left, DISPLAY_HEIGHT, CURSOR_COLOUR);
 }
