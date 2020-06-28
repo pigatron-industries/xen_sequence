b6e97bca3de55ae9c1f363510a1c048d49a4b000
Robert Ellis <rob@pigatron-industries.co.uk>
2020-06-28 17:57:08 +0100

    Removed serial.println

--- src/sequencer/midi/EventCompiler.cpp
+++ src/sequencer/midi/EventCompiler.cpp
@@ -8,21 +8,20 @@ EventCompiler::EventCompiler() {
 }
 
 void EventCompiler::compileEvent(SequenceEvent* event, uint8_t channel) {
-    Serial.println("compileEvent 1");
     clearCompiledEvent(event->getCompiledEvent());
-Serial.println("compileEvent 2");
+    
     if(event->getFlag(SequenceEventFlag::NOTE_ON)) {
         MidiMessage* message = new MidiMessage(event->getDelay(),
             channel, COMMAND_NOTEON, event->getPitch(), event->getVelocity());
         event->getCompiledEvent().messages.add(message);
     }
-Serial.println("compileEvent 3");
+
     if(event->getFlag(SequenceEventFlag::NOTE_OFF)) {
         MidiMessage* message = new MidiMessage(event->getDelay() + event->getGate(),
             channel, COMMAND_NOTEOFF, event->getPitch(), 0);
         event->getCompiledEvent().messages.add(message);
     }
-Serial.println("compileEvent 4");
+
     event->setCompiled();
 }
 
