bb5e6c9ef43c3badaeaf62073bf7a0fdc4bd2888
Robert Ellis <rob@pigatron-industries.co.uk>
2021-03-17 18:25:33 +0000

    Combined encoder driver into single class

--- src/sequencer_src/lib/drivers/RotaryEncoder.h
+++ src/sequencer_src/lib/drivers/RotaryEncoder.h
@@ -1,17 +1,26 @@
 #ifndef RotaryEncoder_h
 #define RotaryEncoder_h
 
-#include "Rotary.h"
+#include <Arduino.h>
+
+// Enable this to emit codes twice per step.
+//#define HALF_STEP
+
+// Enable weak pullups
+#define ENABLE_PULLUPS
+
+// Values returned by 'process'
+// No complete step yet.
+#define DIR_NONE 0x0
+// Clockwise step.
+#define DIR_CW 0x10
+// Anti-clockwise step.
+#define DIR_CCW 0x20
 
 class RotaryEncoder {
 
 public:
-    RotaryEncoder(uint8_t pin1, uint8_t pin2) :
-        rotary(pin1, pin2) {
-        encoderPtr = this;
-        attachInterrupt(pin1, RotaryEncoder::interruptCallback, CHANGE);
-        attachInterrupt(pin2, RotaryEncoder::interruptCallback, CHANGE);
-    }
+    RotaryEncoder(uint8_t pin1, uint8_t pin2);
 
     static void interruptCallback();
 
@@ -28,10 +37,14 @@ public:
 private:
     static RotaryEncoder* encoderPtr;
 
-    Rotary rotary;
     long position = 0;
     long movement = 0;
 
+    unsigned char state;
+    unsigned char pin1;
+    unsigned char pin2;
+
+    unsigned char process();
 };
 
 #endif
